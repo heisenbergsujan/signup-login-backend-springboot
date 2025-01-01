8b7aa59b38c41956a0c47831358f52a5c55b1c76
heisenbergsujan <120708237+heisenbergsujan@users.noreply.github.com>
2025-01-01 17:44:02 +0545

    first commit

---
+++ src/main/java/dev/sujan/signup_login_backend/SignupLoginBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.sujan.signup_login_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupLoginBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupLoginBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sujan/signup_login_backend/SignupLoginBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sujan.signup_login_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignupLoginBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
